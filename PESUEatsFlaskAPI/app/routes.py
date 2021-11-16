035fce887899835c2fa92aa31fb3e874dd06f69a
Vibha Masti <vsmasti@gmail.com>
2021-11-16 14:49:45 +0000

    Added signup endpoint

--- PESUEatsFlaskAPI/app/routes.py
+++ PESUEatsFlaskAPI/app/routes.py
@@ -142,7 +142,6 @@ def get_foodorders():
     )
     return response
 
-    return json.dumps(items, indent=2)
 
 @api_bp.route('/ordertransactions')
 def get_ordertransactions():
@@ -161,27 +160,6 @@ def get_ordertransactions():
         mimetype='application/json'
     )
     return response
-
-    return json.dumps(items, indent=2)
-
-# @api_bp.route('/resetdb')
-# def resetdb():
-#     """
-#     /resetdb
-#     """
-#     con = psycopg2.connect(dbname='pesu_eats', user='postgres', host='localhost')
-#     cur = con.cursor(cursor_factory=RealDictCursor)
-#     cur.execute("SELECT * FROM ORDER_TRANSACTION;")
-#     items = cur.fetchall()
-#     cur.close()
-#     con.close()
-
-#     response = Response(
-#         response=json.dumps(items, indent=2),
-#         mimetype='application/json'
-#     )
-#     return response
-
 
 
 @api_bp.route('/deliveryagent')
@@ -213,4 +191,115 @@ def get_da():
         mimetype='application/json'
     )
     return response
-    return json.dumps(items, indent=2)
+
+# TODO: needs auth
+@api_bp.route('/signup', methods=['POST'])
+def signup():
+    """
+    user details in the form
+    {
+        "name": ,
+        "email": ,
+        "password": , (hashed?)
+        "phone": ,
+        "addr": ,
+    }
+
+    TODO: "loc": , (WKT?)
+    """
+    user_details = request.json
+
+    # Check if json format correct
+    if not ({'email', 'phone', 'name', 'password'} <= user_details.keys()):
+        response = Response(
+            response=json.dumps({
+                "message": "Missing a required parameter (email, phone, name or password)",
+            }), 
+            content_type='application/json',
+            status=400
+        )
+        return response
+
+    phone = None
+    try:
+        phone = int(user_details['phone'])
+    except Exception as e:
+        response = Response(
+            response=json.dumps({
+                "message": "Phone number not a valid phone number",
+            }), 
+            content_type='application/json',
+            status=400
+        )
+        return response
+
+    # Check if email already registered (we will use 
+    # only email and not phone number) for simplicity
+
+    # TODO: add roles to con
+    con = get_pg_conn()
+
+    cur = con.cursor(cursor_factory=RealDictCursor)
+    email = user_details['email']
+    # Check if user already exists
+    query = f"""SELECT * FROM CUSTOMER 
+        WHERE custemail = '{email}';
+    """
+    print(query)
+    cur.execute(query)
+    
+    if cur.fetchone() is None:
+        # TODO: update loc, wallet
+        if 'addr' in user_details.keys():
+            addr = user_details['addr']
+        else:
+            addr = 'null'
+
+        # First create a wallet entry
+        query = f"""INSERT INTO WALLET VALUES (
+            default, 0) RETURNING WID
+        ;"""
+        cur.execute(query)
+        wid = cur.fetchone()['wid']
+        
+
+        query = f"""INSERT INTO CUSTOMER VALUES (
+            default, {wid}, null, {phone}, 
+            '{addr}', '{user_details['name']}', '{user_details['email']}'
+        );"""
+        cur.execute(query)
+        
+        # TODO: add custom roles?
+        query = f"""INSERT INTO APP_USERS VALUES (
+            '{user_details['email']}',
+            '{user_details['password']}',
+            'customer'
+        );"""
+
+        cur.execute(query)
+        con.commit()
+
+
+        query = f"""SELECT CUSTPHONE, CUSTADDR, CUSTNAME, CUSTEMAIL FROM CUSTOMER 
+            WHERE CUSTEMAIL = '{user_details['email']}';
+        """
+        cur.execute(query)        
+        item = cur.fetchone()
+        response = Response(
+            response=json.dumps(item, indent=2),
+            mimetype='application/json',
+            status=200
+        )
+
+    else:
+        response = Response(
+            response=json.dumps({
+                "message": "User already exists. Please sign in.",
+            }), 
+            content_type='application/json',
+            status=400
+        )
+
+    cur.close()
+    con.close()
+    return response
