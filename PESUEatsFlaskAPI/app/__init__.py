961e6eacd66ce3d46ebf67b6a60572c58b2fac9e
Vibha Masti <vsmasti@gmail.com>
2021-11-16 13:29:41 +0000

    Refactored routes

--- PESUEatsFlaskAPI/app/__init__.py
+++ PESUEatsFlaskAPI/app/__init__.py
@@ -1,9 +1,10 @@
 import os
+from psycopg2.extras import RealDictCursor
+
 from app.app import create_app
 from app.helper import appconfig
 
 app = create_app()
-
 
 if appconfig.config['APP_CONFIG']['init'] is True:
     sql_dir = os.path.join(os.path.dirname(os.getcwd()), 'PESUEatsSQL')
