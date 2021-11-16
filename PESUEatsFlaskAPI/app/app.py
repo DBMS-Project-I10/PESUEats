bfda721dc32429c8334a3553326857aed9970a29
Vibha Masti <vsmasti@gmail.com>
2021-11-16 04:21:54 +0000

    Refactored

--- PESUEatsFlaskAPI/app/app.py
+++ PESUEatsFlaskAPI/app/app.py
@@ -5,7 +5,6 @@ import psycopg2
 from psycopg2.extras import RealDictCursor, wait_select
 import json
 
-app = Flask(__name__, instance_relative_config=True)
 
 DEC2FLOAT = psycopg2.extensions.new_type(
     psycopg2.extensions.DECIMAL.values,
@@ -13,111 +12,115 @@ DEC2FLOAT = psycopg2.extensions.new_type(
     lambda value, curs: float(value) if value is not None else None)
 psycopg2.extensions.register_type(DEC2FLOAT)
 
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
 
-@app.route('/')
-def hello():
-    return 'Welcome to PESU Eats API!'
+    @app.route('/')
+    def hello():
+        return 'Welcome to PESU Eats API!'
 
-@app.route('/restaurants')
-def get_restaurants():
-    """
-    /restaurants
-    """
-    con = psycopg2.connect(dbname='pesu_eats', user='postgres', host='localhost')
-    cur = con.cursor(cursor_factory=RealDictCursor)
-    cur.execute("SELECT * FROM RESTAURANT;")
-    items = cur.fetchall()
-    cur.close()
-    con.close()
-    return json.dumps(items, indent=2)
-
-@app.route('/menuitems')
-def get_menuitems():
-    """
-    /menuitems
-    /menuitems?rid={}
-    """
-    con = psycopg2.connect(dbname='pesu_eats', user='postgres', host='localhost')
-    cur = con.cursor(cursor_factory=RealDictCursor)
-
-    rid = request.args.get('rid')
-
-    if rid is None:
-        cur.execute("SELECT * FROM MENU_ITEM;")
-        items = cur.fetchall()
-        cur.close()
-        con.close()
-        return json.dumps(items, indent=2)
-    
-    else:
-        cur.execute(f"SELECT * FROM MENU_ITEM WHERE IinMenuRid = {rid};")
+    @app.route('/restaurants')
+    def get_restaurants():
+        """
+        /restaurants
+        """
+        con = psycopg2.connect(dbname='pesu_eats', user='postgres', host='localhost')
+        cur = con.cursor(cursor_factory=RealDictCursor)
+        cur.execute("SELECT * FROM RESTAURANT;")
         items = cur.fetchall()
         cur.close()
         con.close()
         return json.dumps(items, indent=2)
 
+    @app.route('/menuitems')
+    def get_menuitems():
+        """
+        /menuitems
+        /menuitems?rid={}
+        """
+        con = psycopg2.connect(dbname='pesu_eats', user='postgres', host='localhost')
+        cur = con.cursor(cursor_factory=RealDictCursor)
 
-@app.route('/menuitemincarts')
-def get_menuitemincarts():
-    """
-    /menuitemincarts
-    """
-    con = psycopg2.connect(dbname='pesu_eats', user='postgres', host='localhost')
-    cur = con.cursor(cursor_factory=RealDictCursor)
-    cur.execute("SELECT * FROM MENU_ITEM_IN_CART;")
-    items = cur.fetchall()
-    cur.close()
-    con.close()
-    return json.dumps(items, indent=2)
+        rid = request.args.get('rid')
 
-@app.route('/customers')
-def get_customers():
-    """
-    /customers
-    """
-    con = psycopg2.connect(dbname='pesu_eats', user='postgres', host='localhost')
-    cur = con.cursor(cursor_factory=RealDictCursor)
-    cur.execute("SELECT * FROM CUSTOMER;")
-    items = cur.fetchall()
-    cur.close()
-    con.close()
-    return json.dumps(items, indent=2)
+        if rid is None:
+            cur.execute("SELECT * FROM MENU_ITEM;")
+            items = cur.fetchall()
+            cur.close()
+            con.close()
+            return json.dumps(items, indent=2)
+        
+        else:
+            cur.execute(f"SELECT * FROM MENU_ITEM WHERE IinMenuRid = {rid};")
+            items = cur.fetchall()
+            cur.close()
+            con.close()
+            return json.dumps(items, indent=2)
 
-@app.route('/foodorders')
-def get_foodorders():
-    """
-    /foodorders
-    """
-    con = psycopg2.connect(dbname='pesu_eats', user='postgres', host='localhost')
-    cur = con.cursor(cursor_factory=RealDictCursor)
-    cur.execute("SELECT * FROM FOOD_ORDER;")
-    items = cur.fetchall()
-    cur.close()
-    con.close()
-    return json.dumps(items, indent=2)
 
-@app.route('/ordertransactions')
-def get_ordertransactions():
-    """
-    /ordertransactions
-    """
-    con = psycopg2.connect(dbname='pesu_eats', user='postgres', host='localhost')
-    cur = con.cursor(cursor_factory=RealDictCursor)
-    cur.execute("SELECT * FROM ORDER_TRANSACTION;")
-    items = cur.fetchall()
-    cur.close()
-    con.close()
-    return json.dumps(items, indent=2)
+    @app.route('/menuitemincarts')
+    def get_menuitemincarts():
+        """
+        /menuitemincarts
+        """
+        con = psycopg2.connect(dbname='pesu_eats', user='postgres', host='localhost')
+        cur = con.cursor(cursor_factory=RealDictCursor)
+        cur.execute("SELECT * FROM MENU_ITEM_IN_CART;")
+        items = cur.fetchall()
+        cur.close()
+        con.close()
+        return json.dumps(items, indent=2)
 
-@app.route('/resetdb')
-def get_ordertransactions():
-    """
-    /ordertransactions
-    """
-    con = psycopg2.connect(dbname='pesu_eats', user='postgres', host='localhost')
-    cur = con.cursor(cursor_factory=RealDictCursor)
-    cur.execute("SELECT * FROM ORDER_TRANSACTION;")
-    items = cur.fetchall()
-    cur.close()
-    con.close()
-    return json.dumps(items, indent=2)
+    @app.route('/customers')
+    def get_customers():
+        """
+        /customers
+        """
+        con = psycopg2.connect(dbname='pesu_eats', user='postgres', host='localhost')
+        cur = con.cursor(cursor_factory=RealDictCursor)
+        cur.execute("SELECT * FROM CUSTOMER;")
+        items = cur.fetchall()
+        cur.close()
+        con.close()
+        return json.dumps(items, indent=2)
+
+    @app.route('/foodorders')
+    def get_foodorders():
+        """
+        /foodorders
+        """
+        con = psycopg2.connect(dbname='pesu_eats', user='postgres', host='localhost')
+        cur = con.cursor(cursor_factory=RealDictCursor)
+        cur.execute("SELECT * FROM FOOD_ORDER;")
+        items = cur.fetchall()
+        cur.close()
+        con.close()
+        return json.dumps(items, indent=2)
+
+    @app.route('/ordertransactions')
+    def get_ordertransactions():
+        """
+        /ordertransactions
+        """
+        con = psycopg2.connect(dbname='pesu_eats', user='postgres', host='localhost')
+        cur = con.cursor(cursor_factory=RealDictCursor)
+        cur.execute("SELECT * FROM ORDER_TRANSACTION;")
+        items = cur.fetchall()
+        cur.close()
+        con.close()
+        return json.dumps(items, indent=2)
+
+    @app.route('/resetdb')
+    def resetdb():
+        """
+        /ordertransactions
+        """
+        con = psycopg2.connect(dbname='pesu_eats', user='postgres', host='localhost')
+        cur = con.cursor(cursor_factory=RealDictCursor)
+        cur.execute("SELECT * FROM ORDER_TRANSACTION;")
+        items = cur.fetchall()
+        cur.close()
+        con.close()
+        return json.dumps(items, indent=2)
+
+    return app
--- PESUEatsFlaskAPI/app/auth.py
+++ PESUEatsFlaskAPI/app/auth.py
@@ -4,23 +4,23 @@ import jwt
 
 from app.app import app
 
-def token_required(f):
-    @wraps(f)
-    def decorator(*args, **kwargs):
+# def token_required(f):
+#     @wraps(f)
+#     def decorator(*args, **kwargs):
 
-        token = None
+#         token = None
 
-        if 'x-access-tokens' in request.headers:
-            token = request.headers['x-access-tokens']
+#         if 'x-access-tokens' in request.headers:
+#             token = request.headers['x-access-tokens']
 
-        if not token:
-            return jsonify({'message': 'a valid token is missing'})
+#         if not token:
+#             return jsonify({'message': 'a valid token is missing'})
 
-        try:
-            data = jwt.decode(token, app.config[SECRET_KEY])
-            current_user = Users.query.filter_by(public_id=data['public_id']).first()
-        except:
-            return jsonify({'message': 'token is invalid'})
+#         try:
+#             data = jwt.decode(token, app.config[SECRET_KEY])
+#             current_user = Users.query.filter_by(public_id=data['public_id']).first()
+#         except:
+#             return jsonify({'message': 'token is invalid'})
 
-            return f(current_user, *args, **kwargs)
-    return decorator
+#             return f(current_user, *args, **kwargs)
+#     return decorator
